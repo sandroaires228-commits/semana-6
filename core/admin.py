fc20aeb4af122bf6b37c481455b0cf10fce9540c
sandro aires <sandroaires228@gmail.com>
2026-08-12 13:56:21 -0300

    feat(core): registra e personaliza model Servidor no Django Admin

--- core/admin.py
+++ core/admin.py
@@ -8,5 +8,6 @@ class ServidorAdmin(admin.ModelAdmin):
     """Configura a exibição do model Servidor no painel administrativo."""
     list_display = ("hostname", "endereco_ip", "is_ativo", "data_registro")
     search_fields = ("hostname", "endereco_ip")
-    list_filter = ("is_ativo",)
+    list_filter = ("is_ativo", "data_registro")
+    ordering = ("hostname",)
     readonly_fields = ("data_registro",)
