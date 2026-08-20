d0b6da58fbfa8997eb468e5ec01079cd971becb8
sandro aires <sandroaires228@gmail.com>
2026-08-20 14:20:29 -0300

    fix(auth): configurado LOGIN_REDIRECT_URL e homologacao da Semana 8

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 # core/urls.py
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from main.views import (
     painel_financeiro,
@@ -11,22 +12,19 @@ from main.views import (
 )
 
 urlpatterns = [
+    # Redireciona a raiz "/" para "/painel/"
+    path('', lambda request: redirect('painel')),
+
     path('admin/', admin.site.urls),
-    
-    # Rota MVT Principal
     path('painel/', painel_financeiro, name='painel'),
-    
-    # Exclusão Segura em 2 Passos (Dia 35)
     path('excluir/<int:pk>/', view_excluir_gasto, name='excluir_gasto'),
-    
-    # Registro de Novos Operadores (Dia 34)
     path('registrar-operador/', view_registrar_operador, name='registrar_operador'),
 
-    # Endpoints de API RESTful (Dias 36 e 37)
+    # APIs REST
     path('api/gastos/', api_lista_gastos, name='api_lista_gastos'),
     path('api/gastos/<int:pk>/', api_detalhe_gasto, name='api_detalhe_gasto'),
 
-    # Rotas de Autenticação Nativas (Dia 33)
+    # Autenticação
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
